e3e25e2a85f1ae5b0993e0755c1305c5897e94ed
panzhijun <panzhijun@gomeplus.com>
2019-05-09 10:01:56 +0800

    sssss

--- Cupid/Socket/vc/IMChatViewController.swift
+++ Cupid/Socket/vc/IMChatViewController.swift
@@ -136,7 +136,7 @@ class IMChatViewController: SwiftBaseViewController {
     // 初始化
     init(group : GroupMessage) {
         self.group  = group
-        super.init(nibName: nil, bu/Users/panzhijun/Desktop/Demo/Cupid/Cupidndle: nil)
+        super.init(nibName: nil, bundle: nil)
     }
     
     required init?(coder aDecoder: NSCoder) {
